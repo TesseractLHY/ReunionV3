f8d801b88d4877b39798e9baa316ebbff4145c24
洗玻璃呀 <mcrandom@163.com>
2026-05-31 01:16:55 +0800

    aar

---
+++ android/src/main/java/cn/tesseract/soviet/android/mixin/TestMixin.java
@@ -0,0 +1,13 @@
+package cn.tesseract.soviet.android.mixin;
+
+import android.os.Bundle;
+
+import com.corrodinggames.rts.appFramework.IntroScreen;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(IntroScreen.class)
+public class TestMixin {
+
+}
--- core/src/main/java/cn/tesseract/soviet/mixin/TestMixin.java
+++ core/src/main/java/cn/tesseract/soviet/mixin/TestMixin.java
@@ -1,18 +1,17 @@
 package cn.tesseract.soviet.mixin;
 
 
-import com.corrodinggames.rts.java.Main;
+import com.corrodinggames.rts.game.GameTeam;
+import com.corrodinggames.rts.game.Player;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(Main.class)
-public class TestMixin {
-    @Inject(method = "main", at = @At("HEAD"))
-    private static void main(String[] args, CallbackInfo ci) {
-        ci.cancel();
-        Main.field_8827 = "Mindustry";
+@Mixin(GameTeam.class)
+public abstract class TestMixin extends Player {
+    @Overwrite
+    public void updateTeam(float f) {
+        System.out.println("&" + f);
+        super.updateTeam(f);
     }
 }
